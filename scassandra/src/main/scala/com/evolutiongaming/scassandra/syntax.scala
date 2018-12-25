ce3c3586e165038241fa2cbad16b5ca93be74f7b
Yaroslav Klymko <t3hnar@gmail.com>
2018-12-26 00:09:00 +0100

    improve syntax

--- scassandra/src/main/scala/com/evolutiongaming/scassandra/syntax.scala
+++ scassandra/src/main/scala/com/evolutiongaming/scassandra/syntax.scala
@@ -1,37 +1,27 @@
 package com.evolutiongaming.scassandra
 
-import com.datastax.driver.core.{GettableByIndexData, GettableByNameData, SettableData}
-import com.evolutiongaming.concurrent.CurrentThreadExecutionContext
-import com.google.common.util.concurrent.ListenableFuture
+import com.datastax.driver.core.{GettableByIndexData, GettableByNameData, SettableData, Statement}
+import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
 
-import scala.concurrent.{ExecutionException, Future, Promise}
+import scala.concurrent.{Future, Promise}
 import scala.language.implicitConversions
-import scala.util.{Failure, Try}
 
 object syntax {
 
   implicit class ListenableFutureOps[A](val self: ListenableFuture[A]) extends AnyVal {
 
-    def await(): Try[A] = {
-      val safe = Try(self.get())
-      safe.recoverWith { case failure: ExecutionException => Failure(failure.getCause) }
-    }
-
     def asScala(): Future[A] = {
-      if (self.isDone) {
-        Future.fromTry(await())
-      } else {
-        val promise = Promise[A]
-        val runnable = new Runnable {
-          def run() = promise.complete(await())
-        }
-        self.addListener(runnable, CurrentThreadExecutionContext)
-        promise.future
+      val promise = Promise[A]
+      val callback = new FutureCallback[A] {
+        def onSuccess(result: A) = promise.success(result)
+        def onFailure(cause: Throwable) = promise.failure(cause)
       }
+      Futures.addCallback(self, callback)
+      promise.future
     }
   }
 
-  implicit class SettableDataOps[A <: SettableData[A]](val self: A) extends AnyVal {
+  implicit class ScassandraSettableDataOps[A <: SettableData[A]](val self: A) extends AnyVal {
 
     def encode[B](name: String, value: B)(implicit encode: EncodeByName[B]): A = {
       encode(self, name, value)
@@ -55,7 +45,7 @@ object syntax {
   }
 
 
-  implicit class GettableByNameDataOps(val self: GettableByNameData) extends AnyVal {
+  implicit class ScassandraGettableByNameDataOps(val self: GettableByNameData) extends AnyVal {
 
     def decode[A](name: String)(implicit decode: DecodeByName[A]): A = {
       decode(self, name)
@@ -67,7 +57,7 @@ object syntax {
   }
 
 
-  implicit class GettableByIdxDataOps(val self: GettableByIndexData) extends AnyVal {
+  implicit class ScassandraGettableByIdxDataOps(val self: GettableByIndexData) extends AnyVal {
 
     def decodeAt[A](idx: Int)(implicit decode: DecodeByIdx[A]): A = {
       decode(self, idx)
@@ -76,4 +66,13 @@ object syntax {
 
 
   implicit def toCqlOps[A](a: A) = new ToCql.Ops.IdOps(a)
+
+
+  implicit class ScassandraStatementOps(val self: Statement) extends AnyVal {
+
+    def trace(enable: Boolean): Statement = {
+      if (enable) self.enableTracing()
+      else self.disableTracing()
+    }
+  }
 }
