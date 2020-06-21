fdf38296fd09defb201c83056f1c49a6b238686d
Scala Steward <me@scala-steward.org>
2020-06-21 02:18:10 +0200

    Update cats-helper to 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.4"
   val nel                  = "com.evolutiongaming"    %% "nel"                   % "1.3.4"
   val `cassandra-launcher` = "com.evolutiongaming"    %% "cassandra-launcher"    % "0.0.3"
-  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.0.1"
+  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.0.4"
 
   object Logback {
     private val version = "1.2.3"
