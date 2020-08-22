7307d2638281f8f7b0f093a9d26370aa08b35b96
Scala Steward <me@scala-steward.org>
2020-08-22 06:53:49 +0200

    Update scalatest to 3.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.10.0"
-  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.2.1"
+  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.2.2"
   val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.2"
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.4"
   val nel                  = "com.evolutiongaming"    %% "nel"                   % "1.3.4"
