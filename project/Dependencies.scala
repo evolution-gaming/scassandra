d08bb7c15e99e6e40423323ba008ca2cfb543575
Scala Steward <me@scala-steward.org>
2021-02-17 17:01:50 +0100

    Update cats-effect to 2.3.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
 
   object Cats {
     val core   = "org.typelevel" %% "cats-core"   % "2.1.1"
-    val effect = "org.typelevel" %% "cats-effect" % "2.1.4"
+    val effect = "org.typelevel" %% "cats-effect" % "2.3.3"
   }
 
   object Pureconfig {
