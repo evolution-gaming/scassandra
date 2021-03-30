d8147b7412745f42bf4772f5b042e79a04c631af
Scala Steward <me@scala-steward.org>
2021-03-30 02:05:36 +0200

    Update cats-core to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   }
 
   object Cats {
-    val core   = "org.typelevel" %% "cats-core"   % "2.4.2"
+    val core   = "org.typelevel" %% "cats-core"   % "2.5.0"
     val effect = "org.typelevel" %% "cats-effect" % "2.3.3"
   }
 
