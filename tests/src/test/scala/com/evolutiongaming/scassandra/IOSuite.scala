7068985022676d4ad75c18ede2392736c1faa4ef
Scala Steward <me@scala-steward.org>
2022-01-01 02:50:29 +0100

    Applied Scalafix rule(s) github:typelevel/cats-effect/v3_0_0?sha=v3.0.0

--- tests/src/test/scala/com/evolutiongaming/scassandra/IOSuite.scala
+++ tests/src/test/scala/com/evolutiongaming/scassandra/IOSuite.scala
@@ -1,12 +1,13 @@
 package com.evolutiongaming.scassandra
 
 import cats.Parallel
-import cats.effect.{Concurrent, ContextShift, IO, Timer}
+import cats.effect.{Concurrent, IO}
 import com.evolutiongaming.scassandra.util.FromGFuture
 import org.scalatest.Succeeded
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import cats.effect.Temporal
 
 object IOSuite {
   val Timeout: FiniteDuration = 10.seconds
@@ -15,7 +16,7 @@ object IOSuite {
 
   implicit val contextShiftIO: ContextShift[IO] = IO.contextShift(executor)
   implicit val concurrentIO: Concurrent[IO] = IO.ioConcurrentEffect
-  implicit val timerIO: Timer[IO] = IO.timer(executor)
+  implicit val timerIO: Temporal[IO] = IO.timer(executor)
   implicit val parallelIO: Parallel[IO] = IO.ioParallel
   implicit val fromGFutureIO: FromGFuture[IO] = FromGFuture.lift[IO]
 
