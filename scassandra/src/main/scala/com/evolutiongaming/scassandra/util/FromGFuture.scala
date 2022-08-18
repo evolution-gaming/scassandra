da3d3f4a9962c0149fb55414a16de8aabc36308b
Denys Fakhritdinov <dfakhritdinov@evolution.com>
2022-08-18 14:43:03 +0200

    make change backward compatible

--- scassandra/src/main/scala/com/evolutiongaming/scassandra/util/FromGFuture.scala
+++ scassandra/src/main/scala/com/evolutiongaming/scassandra/util/FromGFuture.scala
@@ -9,6 +9,8 @@ import com.google.common.util.concurrent.{
   ListenableFuture
 }
 
+import java.util.concurrent.Executor
+
 trait FromGFuture[F[_]] {
 
   def apply[A](future: => ListenableFuture[A]): F[A]
@@ -17,6 +19,31 @@ trait FromGFuture[F[_]] {
 object FromGFuture {
 
   def apply[F[_]](implicit F: FromGFuture[F]): FromGFuture[F] = F
+
+  @deprecated("use FromGFuture.lift(implicit F: Async[F]) instead", "4.0.1")
+  def lift[F[_]: Async](implicit executor: Executor): FromGFuture[F] = {
+
+    new FromGFuture[F] {
+
+      def apply[A](future: => ListenableFuture[A]) = {
+        for {
+          ec <- Async[F].executionContext
+          executor <- ExecutionContextExecutorServiceFactory(ec).pure[F]
+          future <- Sync[F].delay {
+            future
+          }
+          result <- Async[F].async_[A] { callback =>
+            val futureCallback = new FutureCallback[A] {
+              def onSuccess(a: A) = callback(a.asRight)
+
+              def onFailure(e: Throwable) = callback(e.asLeft)
+            }
+            Futures.addCallback(future, futureCallback, executor)
+          }
+        } yield result
+      }
+    }
+  }
 
   implicit def lift[F[_]: Async]: FromGFuture[F] = {
 
