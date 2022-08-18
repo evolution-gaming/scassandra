ead388ce8e2ee57e810749960cd0c28aa66c2251
Denys Fakhritdinov <denys.fakhritdinov@gmail.com>
2022-08-18 15:19:02 +0200

    use Async.executionContext instead of explicit parameter (#159)

--- scassandra/src/main/scala/com/evolutiongaming/scassandra/util/FromGFuture.scala
+++ scassandra/src/main/scala/com/evolutiongaming/scassandra/util/FromGFuture.scala
@@ -1,10 +1,15 @@
 package com.evolutiongaming.scassandra.util
 
-import java.util.concurrent.Executor
-
 import cats.effect.{Async, Sync}
 import cats.implicits._
-import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
+import com.evolutiongaming.concurrent.ExecutionContextExecutorServiceFactory
+import com.google.common.util.concurrent.{
+  FutureCallback,
+  Futures,
+  ListenableFuture
+}
+
+import java.util.concurrent.Executor
 
 trait FromGFuture[F[_]] {
 
@@ -15,13 +20,39 @@ object FromGFuture {
 
   def apply[F[_]](implicit F: FromGFuture[F]): FromGFuture[F] = F
 
-
-  implicit def lift[F[_] : Async](implicit executor: Executor): FromGFuture[F] = {
+  @deprecated("use FromGFuture.lift(implicit F: Async[F]) instead", "4.0.1")
+  def lift[F[_]: Async](implicit executor: Executor): FromGFuture[F] = {
 
     new FromGFuture[F] {
 
       def apply[A](future: => ListenableFuture[A]) = {
         for {
+          ec <- Async[F].executionContext
+          executor <- ExecutionContextExecutorServiceFactory(ec).pure[F]
+          future <- Sync[F].delay {
+            future
+          }
+          result <- Async[F].async_[A] { callback =>
+            val futureCallback = new FutureCallback[A] {
+              def onSuccess(a: A) = callback(a.asRight)
+
+              def onFailure(e: Throwable) = callback(e.asLeft)
+            }
+            Futures.addCallback(future, futureCallback, executor)
+          }
+        } yield result
+      }
+    }
+  }
+
+  implicit def liftFromAsync[F[_]: Async]: FromGFuture[F] = {
+
+    new FromGFuture[F] {
+
+      def apply[A](future: => ListenableFuture[A]) = {
+        for {
+          ec <- Async[F].executionContext
+          executor <- ExecutionContextExecutorServiceFactory(ec).pure[F]
           future <- Sync[F].delay { future }
           result <- Async[F].async_[A] { callback =>
             val futureCallback = new FutureCallback[A] {
