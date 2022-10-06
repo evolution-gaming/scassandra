16d3acb6106d68f4bcc99174490330d6b97e43b4
Yaroslav Klymko <t3hnar@gmail.com>
2022-10-06 10:50:21 +0200

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,30 +2,30 @@ import sbt._
 
 object Dependencies {
 
-  val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.11.0"
-  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.2.10"
+  val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.11.3"
+  val scalatest            = "org.scalatest"          %% "scalatest"             % "3.2.14"
   val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.3"
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.4"
   val nel                  = "com.evolutiongaming"    %% "nel"                   % "1.3.4"
   val `cassandra-launcher` = "com.evolutiongaming"    %% "cassandra-launcher"    % "0.0.4"
-  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.7.2"
+  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "2.8.0"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "0.2.1"
 
   object Logback {
-    private val version = "1.2.7"
+    private val version = "1.4.3"
     val core    = "ch.qos.logback" % "logback-core"    % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
 
   object Slf4j {
-    private val version = "1.7.32"
+    private val version = "1.7.36"
     val api                = "org.slf4j" % "slf4j-api"        % version
     val `log4j-over-slf4j` = "org.slf4j" % "log4j-over-slf4j" % version
   }
 
   object Cats {
-    val core   = "org.typelevel" %% "cats-core"   % "2.7.0"
-    val effect = "org.typelevel" %% "cats-effect" % "2.5.4"
+    val core   = "org.typelevel" %% "cats-core"   % "2.8.0"
+    val effect = "org.typelevel" %% "cats-effect" % "2.5.5"
   }
 
   object Pureconfig {
