9f9a446a36c581804a095fc41eb01a507160924e
Scala Steward <scala_steward@virtuslab.com>
2023-06-14 15:31:47 +0000

    Update cats-helper to 3.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.4"
   val nel                  = "com.evolutiongaming"    %% "nel"                   % "1.3.4"
   val `cassandra-launcher` = "com.evolutiongaming"    %% "cassandra-launcher"    % "0.0.4"
-  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "3.2.0"
+  val `cats-helper`        = "com.evolutiongaming"    %% "cats-helper"           % "3.6.0"
   val sstream              = "com.evolutiongaming"    %% "sstream"               % "1.0.1"
 
   object Logback {
