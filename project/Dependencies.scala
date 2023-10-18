d452bde7924ea6c98a54f4473dd80f6146fe2da0
Scala Steward <scala_steward@virtuslab.com>
2023-10-18 16:46:08 +0000

    Revert commit(s) 462f731

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val `cassandra-driver`   = "com.datastax.cassandra"  % "cassandra-driver-core" % "3.11.3"
   val scalatest            = "org.scalatest"          %% "scalatest"             % "3.2.16"
-  val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.4"
+  val `executor-tools`     = "com.evolutiongaming"    %% "executor-tools"        % "1.0.3"
   val `config-tools`       = "com.evolutiongaming"    %% "config-tools"          % "1.0.4"
   val nel                  = "com.evolutiongaming"    %% "nel"                   % "1.3.4"
   val `cassandra-launcher` = "com.evolutiongaming"    %% "cassandra-launcher"    % "0.0.4"
