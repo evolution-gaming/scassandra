161f2158f96ceaada9473fde754001169bf39840
Ruslans Tarasovs <49794769+rtar@users.noreply.github.com>
2023-10-20 13:57:04 +0300

    Add scaladoc and a typo fix to ConfigReaderFromEnum (#222)

--- scassandra/src/main/scala/com/evolutiongaming/scassandra/util/ConfigReaderFromEnum.scala
+++ scassandra/src/main/scala/com/evolutiongaming/scassandra/util/ConfigReaderFromEnum.scala
@@ -6,12 +6,33 @@ import pureconfig.{ConfigCursor, ConfigReader}
 
 import scala.reflect.ClassTag
 
+/** Provides [[ConfigReader]] for Java enums.
+  *
+  * Example:
+  * {{{
+  * implicit val configReaderProtocolVersion: ConfigReader[ProtocolVersion] =
+  *   ConfigReaderFromEnum(ProtocolVersion.values())
+  * }}}
+  *
+  * The main difference between this class and build-in reader in pureconfig is
+  * that this one ignores enum case when comparing the values, avoding the
+  * runtime problems when somebody configured `v4` instead of `V4` for example.
+  *
+  * Note, that this class does not detect if there are duplicate enum values,
+  * i.e. if both `v4` and `V4` is a part of this enum, and will just return a
+  * first one encountered.
+  */
 object ConfigReaderFromEnum {
 
+  /** Creates an instance of [[ConfigReader]] for specific Java enum.
+    *
+    * @param values List of all enum values, i.e. returned by `enum.values()` call.
+    * @param tag [[ClassTag]] needed to report a class name on failure.
+    */
   def apply[A <: Enum[A]](values: Array[A])(implicit tag: ClassTag[A]): ConfigReader[A] = {
     cursor: ConfigCursor => {
 
-      def consistencyLevel(str: String) = {
+      def fromString(str: String) = {
         values
           .find { _.name equalsIgnoreCase str }
           .fold {
@@ -22,7 +43,7 @@ object ConfigReaderFromEnum {
 
       for {
         s <- cursor.asString
-        r <- consistencyLevel(s)
+        r <- fromString(s)
       } yield r
     }
   }
