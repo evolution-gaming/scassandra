cbf2d9bc97962c321a0d934c3f438cc67b10520a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-29 13:55:30 +0200

    Update sstream to 1.3.0 (#353)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val nel = "com.evolutiongaming" %% "nel" % "1.3.5"
   val `testcontainers-cassandra` = "com.dimafeng" %% "testcontainers-scala-cassandra" % "0.44.1"
   val `cats-helper` = "com.evolutiongaming" %% "cats-helper" % "3.12.2"
-  val sstream = "com.evolutiongaming" %% "sstream" % "1.1.0"
+  val sstream = "com.evolutiongaming" %% "sstream" % "1.3.0"
 
   object Logback {
     private val version = "1.5.37"
