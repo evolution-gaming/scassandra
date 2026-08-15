c46bd3e0f94dc7b15d1f25a7a2b9a469cdf136ce
Scala Steward <scala_steward@virtuslab.com>
2026-08-15 14:57:01 +0000

    Update logback-classic, logback-core to 1.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val sstream = "com.evolutiongaming" %% "sstream" % "1.3.0"
 
   object Logback {
-    private val version = "1.6.2"
+    private val version = "1.6.3"
     val core = "ch.qos.logback" % "logback-core" % version
     val classic = "ch.qos.logback" % "logback-classic" % version
   }
